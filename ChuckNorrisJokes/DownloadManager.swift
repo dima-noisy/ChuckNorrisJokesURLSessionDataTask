11ce521ae6912d640284e27310f5e0720482759f
Dmitry Poryadin <dporyadin11@gmail.com>
2024-11-10 10:54:00 +0300

    initial commit

---
+++ ChuckNorrisJokes/DownloadManager.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+//https://api.chucknorris.io/jokes/random
+
+enum DownloadError: Error {
+    case errorAnswerCode
+    case dataIsNil
+    case error(Error)
+}
+
+class DownloadManager {
+    func downloadRandomJoke(completion: ((Result<String, DownloadError>) -> Void)?) {
+    //func downloadRandomJoke(completion: ((_ jokeText: String?) -> Void)? ) {
+        let session = URLSession.shared
+        let url = URL(string: "https://api.chucknorris.io/jokes/random")
+        let task = session.dataTask(with: url!) { data, responce, error in
+            if error != nil {
+                print(error!.localizedDescription)
+                completion?(.failure(.error(error!)))
+                return
+            }
+            
+            if let httpUrlResponce = responce as? HTTPURLResponse, httpUrlResponce.statusCode != 200 {
+                print("Error, code of responce: \(httpUrlResponce.statusCode)")
+                completion?(.failure(.errorAnswerCode))
+                return
+            }
+            
+            guard let data else {
+                print("No Data")
+                
+                completion?(.failure(.dataIsNil))
+                return
+            }
+            //print("get correct answer")
+            
+            do {
+                let answer = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                let jokeText = answer?["value"] as? String ?? "😘"
+                completion?(.success(jokeText))
+//                print(jokeText)
+            } catch {
+                print("Error of parsing: \(error.localizedDescription)")
+            }
+            
+        }
+        task.resume()
+    }
+}
--- ChuckNorrisJokes/ViewController.swift
+++ ChuckNorrisJokes/ViewController.swift
@@ -1,19 +1,40 @@
-//
-//  ViewController.swift
-//  ChuckNorrisJokes
-//
-//  Created by Дмитрий Порядин on 07.11.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var downloadManager = DownloadManager()
 
+    @IBOutlet weak var labelJoke: UILabel!
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBOutlet weak var reftrshButton: UIButton!
+    @IBAction func pushRefreshButton(_ sender: Any) {
+        indicator.startAnimating()
+        reftrshButton.isEnabled = false
+        downloadManager.downloadRandomJoke { [weak self] result in
+            
+            switch result {
+            case .success(let jokeText):
+                DispatchQueue.main.async {
+                    self?.labelJoke.text = jokeText
+                }
+            case .failure(let error):
+                
+                print(error.localizedDescription)
+                break
+            }
+            DispatchQueue.main.async {
+                self?.indicator.stopAnimating()
+                self?.reftrshButton.isEnabled = true
+            }
+        }
+    }
+    
 }
 
